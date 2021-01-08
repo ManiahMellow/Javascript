8c5c22db69cfb3a2c1f984ffdba8306fbafd45e9
ManiahMellow <jellakus@gmail.com>
2021-01-08 11:34:38 +0700

    JAVASCRIPT #1
    Tugas Javascript #1

---
+++ Javascript #1/js/variabel.js
@@ -0,0 +1,15 @@
+let angka = 100;
+angka = 50;
+
+let word = "SEMANGAT YA BELAJARNYA! :)";
+
+const data = 1000;
+
+console.log(angka);
+console.log(word);
+
+console.log(data);
+
+let isi = 6;
+
+console.log(typeof isi);
