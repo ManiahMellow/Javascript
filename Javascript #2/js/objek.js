2824299e62322869f87c7381fe13afafee191445
ManiahMellow <jellakus@gmail.com>
2021-01-15 12:03:04 +0700

    JAVASCRIPT #2
    Tugas Javascipt #2

---
+++ Javascript #2/js/objek.js
@@ -0,0 +1,20 @@
+const objek = {
+    nama : "ellow",
+    telp : 087729301815,
+
+    tempat : ["Sidoarjo", "Surabaya", "Malang", "Bali", "Kediri"],
+
+    coba : function () {
+        return "JANGAN LUPA SENYUM!";
+    },
+
+    bolean : true,
+
+    "nothing" : "ahahahahayaflu"
+
+};
+
+console.log(objek.tempat[2]);
+
+console.log(objek.nothing);
+console.log(objek["nothing"]);
